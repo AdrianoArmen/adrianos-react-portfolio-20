a95ba63857c5c447e5320502375036c908e5aca5
AdrianoA <adriano.armendaris@gmail.com>
2021-08-04 20:09:53 -0500

    deleted searchbar in projects not working properly

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
 import SectionTitle from '../components/SectionTitle';
 import ProjectsInfo from '../assets/data/projects';
 import ProjectItem from '../components/ProjectItem';
@@ -35,49 +34,16 @@ const ProjectStyle = styled.div`
   .projects__searchBar .searchIcon path {
     color: var(--deep-dark);
   }
-  @media only screen and (max-width: 768px) {
-    .projects__searchBar,
-    .projects__searchBar form,
-    .projects__searchBar input {
-      width: 100%;
-    }
-  }
 `;
 
 export default function Projects() {
-  const [searchText, setSearchText] = useState('');
-  const [projectsData, setProjectsData] = useState(ProjectsInfo);
-  useEffect(() => {
-    if (searchText === '') return;
-    setProjectsData(() =>
-      ProjectsInfo.filter((item) =>
-        item.name.toLowerCase().match(searchText.toLowerCase())
-      )
-    );
-  }, [searchText]);
-  const handleChange = (e) => {
-    e.preventDefault();
-    setSearchText(e.target.value);
-    if (!e.target.value.length > 0) {
-      setProjectsData(ProjectsInfo);
-    }
-  };
+  const [projectsData] = useState(ProjectsInfo);
+
   return (
     <>
       <ProjectStyle>
         <div className="container">
           <SectionTitle heading="Projects" subheading="My recent work" />
-          <div className="projects__searchBar">
-            <form>
-              <input
-                type="text"
-                value={searchText}
-                onChange={handleChange}
-                placeholder="Project Name"
-              />
-              <MdSearch className="searchIcon" />
-            </form>
-          </div>
           <div className="projects__allItems">
             {projectsData.map((item) => (
               <ProjectItem
