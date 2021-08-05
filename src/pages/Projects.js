c9580315f079102d0b514d1788e31f2f758d5539
AdrianoA <adriano.armendaris@gmail.com>
2021-08-04 21:52:11 -0500

    removed searchbar styles

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,28 +12,6 @@ const ProjectStyle = styled.div`
     gap: 5rem;
     margin-top: 5rem;
   }
-  .projects__searchBar {
-    position: relative;
-    width: 300px;
-    margin-top: 5rem;
-  }
-  .projects__searchBar input {
-    width: 100%;
-    font-size: 2rem;
-    padding: 0.8rem;
-    color: var(--black);
-    border-radius: 6px;
-    outline: none;
-    border: none;
-  }
-  .projects__searchBar .searchIcon {
-    position: absolute;
-    width: 2rem;
-    right: 1rem;
-  }
-  .projects__searchBar .searchIcon path {
-    color: var(--deep-dark);
-  }
 `;
 
 export default function Projects() {
